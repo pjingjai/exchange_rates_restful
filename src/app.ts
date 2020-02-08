8adebeb2183f2aab33f303fea496797c87a193a5
pun <pjingjai@gmail.com>
2020-02-08 15:42:39 +0700

    use readdir()

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,25 @@
 const Koa = require("koa");
 const Router = require("koa-router");
 const axios = require("axios");
-const fs = require('fs-extra');
+const fs = require("fs");
+const fsx = require("fs-extra");
 const koaBody = require("koa-body");
 const path = require("path");
+const { promisify } = require("util");
 
 const app = new Koa();
 const router = new Router();
 
-let mainExchangeRatesFile = "exchange_rates_data";
-const textFileDir = path.join(__dirname, "../uploads");
+const readdir = promisify(fs.readdir);
+
+const _MAIN = "exchange_rates_data"
+let currentExchangeRatesFile = _MAIN;
+const fileDir = path.join(__dirname, "../files");
 
 const fetchExchangeRatesAPIThenWriteFile = async () => {
     try {
         const response = await axios.get("https://api.exchangeratesapi.io/latest?base=USD");
-        await fs.writeFile(path.join(textFileDir, "exchange_rates_data"), JSON.stringify(response.data), "utf8");
+        await fsx.writeFile(path.join(fileDir, "exchange_rates_data"), JSON.stringify(response.data), "utf8");
         console.log("Saved!");
     } catch (err) {
         console.log(err);
@@ -37,7 +42,8 @@ router
         "/",
         async (ctx: any, next: any) => {
             try {
-                ctx.body = await fs.readFile(path.join(textFileDir, mainExchangeRatesFile), "utf8");
+                console.log(currentExchangeRatesFile)
+                ctx.body = await fsx.readFile(path.join(fileDir, currentExchangeRatesFile), "utf8");
             } catch (err) {
                 console.log(err);
             }
@@ -60,7 +66,7 @@ router
                     throw new Error("invalid params length");
                 }
 
-                const exchange = JSON.parse(await fs.readFile(path.join(textFileDir, mainExchangeRatesFile), "utf8"));
+                const exchange = JSON.parse(await fsx.readFile(path.join(fileDir, currentExchangeRatesFile), "utf8"));
 
                 const from: string = (ctx.params.fromCur).toUpperCase();
                 const to: string = (ctx.params.toCur).toUpperCase();
@@ -71,6 +77,7 @@ router
                 ctx.body = result;
             } catch (err) {
                 console.log(err);
+                ctx.status = err.status || 400;
             }
 
             await next();
@@ -82,22 +89,19 @@ router
         async (ctx: any, next: any) => {
             try {
                 if (ctx.request.files.file.type !== "application/octet-stream") {
-                    ctx.status = 400;
-                    ctx.body = "file type not allowed";
-                    throw new Error("file type not allowed");
+                    ctx.throw(400, "file type not allowed");
                 }
                 if (Object.keys(ctx.request.files).length !== 1) {
-                    ctx.status = 400;
-                    ctx.body = "can only upload 1 file at a time";
-                    throw new Error("can only upload 1 file at a time");
+                    ctx.throw(400, "can only upload 1 file at a time");
                 }
 
                 const oldPath = Object.values(ctx.request.files)[0].path;
-                const textFileName = ctx.request.body.filename  || String(Date.now());
-                await fs.rename(oldPath, path.join(textFileDir, textFileName));
+                const textFileName = ctx.request.body.filename || String(Date.now());
+                await fsx.rename(oldPath, path.join(fileDir, textFileName));
                 ctx.status = 201;
             } catch (err) {
                 console.log(err);
+                ctx.status = err.status || 400;
             }
 
 
@@ -108,8 +112,32 @@ router
     .put(
         "/:filename",
         async (ctx: any, next: any) => {
-            mainExchangeRatesFile = ctx.params.filename;
-            ctx.body = "Main: " + mainExchangeRatesFile;
+            try {
+                let filename: string;
+                if (typeof (ctx.params.filename) !== "string") {
+                    ctx.status = 400;
+                    ctx.body = "invalid params type";
+                    throw new Error("invalid params type");
+                }
+
+                if (ctx.params.filename === "_MAIN") {
+                    filename = _MAIN
+                } else {
+                    filename = ctx.params.filename;
+                }
+                if ((await readdir(fileDir)).includes(filename)) {
+                    currentExchangeRatesFile = filename;
+                } else {
+                    ctx.status = 400;
+                    ctx.body = "file dose not exist";
+                    throw new Error("file dose not exist");
+                }
+
+                ctx.body = "Main: " + currentExchangeRatesFile;
+            } catch (err) {
+                console.log(err);
+                ctx.status = err.status || 400;
+            }
 
             await next();
         }
@@ -117,10 +145,22 @@ router
     .delete(
         "/:filename",
         async (ctx: any, next: any) => {
-            const textFileName = ctx.params.filename;
-            await fs.remove(path.join(textFileDir, textFileName));
-            ctx.status = 200;
-            ctx.body = textFileName + " Deleted!";
+            if (typeof (ctx.params.filename) !== "string") {
+                ctx.status = 400;
+                ctx.body = "invalid params type";
+                throw new Error("invalid params type");
+            }
+            // Delete
+            if ((await readdir(fileDir)).includes(ctx.params.filename)) {
+                const textFileName = ctx.params.filename;
+                await fsx.remove(path.join(fileDir, textFileName));
+                ctx.status = 200;
+                ctx.body = textFileName + " Deleted!";
+            } else {
+                ctx.status = 400;
+                ctx.body = "file dose not exist";
+                throw new Error("file dose not exist");
+            }
 
             await next();
         }
